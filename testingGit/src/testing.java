0bb479c7e8cb965f82b139410eb46d0fb1d5824e
AahilD <Aahil@10.13.169.116>
2019-01-25 12:37:25 -0700

    Hey

--- testingGit/src/testing.java
+++ testingGit/src/testing.java
@@ -4,6 +4,7 @@ public class testing
 	public static void main(String[] args)
 	{
 		System.out.println("you have succesffully pulled this project from UC-G22 repository!");
+		System.out.println("Testing");
 		int x = 0;
 		
 	}
