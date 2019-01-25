ba8de938d11d4c5780011c391791dae2dc45b3a9
manucote <manu.cote1@gmail.com>
2019-01-25 14:39:34 -0700

    supressed a warning

--- GuessingGame/GuessingGame.java
+++ GuessingGame/GuessingGame.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 public class GuessingGame {
 	
 	public static boolean getAndCheckGuess(int numberToGuess) {
+		@SuppressWarnings("resource")
 		Scanner reader = new Scanner(System.in);
 		System.out.print("Enter a guess: ");
 		int guess = reader.nextInt();
