9dc01592a7e935a982a4ea87b36941bb5606aaf3
Aahil <aahild07@gmail.com>
2019-02-03 13:12:54 -0700

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,94 @@
+public class BankAccount {
+
+	private double balance;
+	private String accountNumber;
+	
+
+// Constructors //	
+
+// Default	
+	public BankAccount () {
+		
+		balance = 0;
+		accountNumber = "0001";
+		
+	}
+	
+// Takes start balance 
+	public BankAccount (Double startBalance) {
+		
+		if (startBalance >= 0) {
+			balance = startBalance;
+		
+		} else {
+			balance = startBalance;
+			System.out.println("Unexpected balance"); 
+		}
+		accountNumber = "0001";
+	} 
+	
+		
+// Takes start balance and account number	
+	public BankAccount (Double startBalance, String accNum) {
+		
+		balance = startBalance;
+		accountNumber = accNum;
+	
+	}	
+
+
+// Mutators (Setters) //
+
+// Subtracts withdraw amount from balance		
+	public void withdraw(Double withdraw) {
+		
+		if (withdraw >= 0) {
+			if (balance >= withdraw) {
+				this.balance -= withdraw;
+			} else {
+				System.out.println("Insufficient funds.");
+			}
+		} else {
+			System.out.println("Can not withdraw a negative number.");
+		}	
+	}
+
+// Adds deposit amount to balance	
+	public void deposit(Double deposit) {
+		
+		if (deposit >= 0) {
+			this.balance += deposit;
+		} else {
+			System.out.println("Can not withdraw a negative number.");
+		}
+	}
+
+	
+// Accessor (Getters) //	
+
+// Returns instance variable "balance"	
+	public double getBalance() {
+		
+		return balance;	
+		
+	}
+	
+// Returns instance variable "accountNumber"	
+	public String getAccountNumber() {
+		
+		return accountNumber;
+		
+	}
+
+// Returns the "accountNumber" and "balance"	
+	public String toString() {
+		
+		return String.format(accountNumber + ": " + balance);
+	}
+
+	
+// Main function //
+	public static void main(String[] args) {	
+		
+	}
+}
