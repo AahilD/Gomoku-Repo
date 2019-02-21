6ecb72b90d3344795d918c48a223af0cd1a43621
leslienguyen <leslienguyen@10.44.26.82>
2019-02-21 12:37:57 -0700

    Start of the Player class

--- Gomoku/src/broker/Player.java
+++ Gomoku/src/broker/Player.java
@@ -12,4 +12,38 @@ package broker;
 public class Player
 {
 	// TODO implement variables, constructor(s), getters, setters, and other methods
+	private String userName;
+	private int winCount;
+	private int loseCount;
+	private int drawCount;
+	
+	//Getter
+	public String getUserName() {
+		return userName;
+	}
+	public int getWinCount() {
+		return winCount;
+	}
+	public int getLoseCount() {
+		return loseCount;
+	}
+	public int getDrawCount() {
+		return drawCount;
+	}
+	
+	
+	//Setter
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public void setWinCount(int winCount) {
+		this.winCount = winCount;
+	}
+	public void setLoseCount(int loseCount) {
+		this.loseCount = loseCount;
+	}
+	public void setDrawCount(int drawCount) {
+		this.drawCount = drawCount;
+	}
+	
 }
