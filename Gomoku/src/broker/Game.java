83f42606d43da05fc8c7af15f2f0d6a08fdf6eea
manucote <manu.cote1@gmail.com>
2019-02-21 12:57:07 -0700

    added empty Game object class

---
+++ Gomoku/src/broker/Game.java
@@ -0,0 +1,19 @@
+package broker;
+
+/**
+ * @author GROUP22
+ *
+ * This class represents the current state of the game being played
+ */
+public class Game
+{
+	//playerOne: object<Player>
+	//playerTwo: object<Player>
+	//board: object<board>
+	//round: int
+	
+	// newgame(): new Board(): void
+		// -- increment round
+	// makemove(Player): boolean
+		// returns true if this move was the winning move 
+}
