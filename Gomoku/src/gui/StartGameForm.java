27bd525561a2606f4106bc1e5f45a6ed13e63a27
manucote <manu.cote1@gmail.com>
2019-02-21 12:41:50 -0700

    added a gui class for the initial pop up window before game starts

---
+++ Gomoku/src/gui/StartGameForm.java
@@ -0,0 +1,18 @@
+package gui;
+
+/**
+ * @author GROUP 22
+ *
+ * This class will be a gui instance
+ * that will load when the game initially starts
+ * it will prompt for two usernames to be entered
+ * (for now it will assign their pieces to be Player1 = white
+ * player2 = black)
+ * upon clicking the "start game" button the main gui will appear
+ * with a fresh new game for player one to start.
+ * 
+ */
+public class StartGameForm
+{
+
+}
