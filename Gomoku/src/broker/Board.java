d4993d7fd12c22bc1afffb1c697dd3682824c16d
Emily Pang <emilypang23@hotmail.com>
2019-02-22 10:46:33 -0700

    updated with separate methods except for diagonals

--- Gomoku/src/broker/Board.java
+++ Gomoku/src/broker/Board.java
@@ -56,107 +56,63 @@ public class Board
 	 * @param piece
 	 * @return
 	 */
-	public boolean hasWon(char piece)
-	{
+	private boolean verifyHorizontal(char piece) {
 		boolean fiveInARow = false;
-		// check rows and columns
-		int count = 0;
-		// Check horizontal
+		int i = 0; 
 		for (int row = 0; row < board.length && !fiveInARow; row++)
 		{
 			for (int col = 0; col < board[row].length; col++)
 			{
 				if (board[row][col].getPieceColour() == piece)
 				{
-					count++;
-				} else if (count >= 5)
+					i++;
+				} else if (i >= 5)
 				{
 					fiveInARow = true;
 				} else
 				{
 					fiveInARow = false;
-					count = 0;
+					i = 0;
 				}
 			}
 		}
-		// Check Vertical
+		return fiveInARow; 
+	}
+	private boolean verifyVertical(char piece) {
+		boolean fiveInARow = false; 
+		int i = 0; 
 		for (int r = 0; r < board.length && !fiveInARow; r++)
 		{
 			for (int c = 0; c < board[r].length; c++)
 			{
 				if (board[r][c].getPieceColour() == piece)
 				{
-					count++;
+					i++;
 				} else
 				{
-					count = 0;
+					i = 0;
 				}
 			}
 		}
-		if (count >= 5)
+		if (i >= 5)
 		{
 			fiveInARow = true;
 		} else
 		{
 			fiveInARow = false;
 		}
-		// Check Diagonal (Check in all 4 direction, and check up in the same
-		// direction)
-		// Diagonal Upwards
-		for (int ro = 0; ro < board.length && !fiveInARow; ro++)
-		{
-			if (board[ro][ro + 1].getPieceColour() == piece)
-			{
-				count++;
-			} else if (count >= 5)
-			{
-				fiveInARow = true;
-			} else
-			{
-				fiveInARow = false;
-				count = 0;
-			}
-			if (board[ro][ro - 1].getPieceColour() == piece)
-			{
-				count++;
-			} else if (count >= 5)
-			{
-				fiveInARow = true;
-			} else
-			{
-				fiveInARow = false;
-				count = 0;
-			}
-		}
-		// Diagonal Downwards//
-		for (int co = 0; co < board.length && !fiveInARow; co++)
-		{
-			if (board[co + 1][co].getPieceColour() == piece)
-			{
-				count++;
-			} else if (count >= 5)
-			{
-				fiveInARow = true;
-			} else
-			{
-				fiveInARow = false;
-			}
-			if (board[co - 1][co].getPieceColour() == piece)
-			{
-				count++;
-			} else if (count >= 5)
-			{
-				fiveInARow = true;
-			} else
-			{
-				fiveInARow = false;
-			}
-		}
-
+		return fiveInARow; 
+	}
+	private boolean verifyDiagonalLeft(char piece) {
+		boolean fiveInARow = false;
+		return fiveInARow;
+	}
+	private boolean verifyDiagonalRight(char piece) {
+		boolean fiveInARow = false;
 		return fiveInARow;
 	}
 
-	public boolean boardFull()
+	private boolean boardFull()
 	{
 		boolean full = true;
 		for (int row = 0; row < 19 && full; row++)
@@ -174,6 +130,10 @@ public class Board
 
 	public boolean gameOver()
 	{
-		return hasWon('w') || hasWon('b') || boardFull();
+		return verifyVertical('w')||verifyVertical('b')||
+				verifyHorizontal('w') || verifyHorizontal('b') ||
+				verifyDiagonalLeft('w')||verifyDiagonalLeft('b')||
+				verifyDiagonalRight('w')||verifyDiagonalRight('b')||
+				boardFull();
 	}
 }
