6ba2f57af4a0e98e38613deab53efcd234891404
manucote <manu.cote1@gmail.com>
2019-02-22 13:24:41 -0700

    added default constructor to IllegalMove Exception class

--- Gomoku/src/broker/IllegalMove.java
+++ Gomoku/src/broker/IllegalMove.java
@@ -1,5 +1,10 @@
 package broker;
 
-public class IllegalMove extends Exception {
+public class IllegalMove extends Exception
+{
+	public IllegalMove()
+	{
+		super("This square is not playable");
+	}
 
 }
