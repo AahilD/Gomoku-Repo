5fde9882a2b6d9e8d99ad2892f971c92beef0a08
leslienguyen <Leslie_d_n@hotmail.com>
2019-02-23 13:11:36 -0700

    updated javadoc

--- Gomoku/src/broker/Piece.java
+++ Gomoku/src/broker/Piece.java
@@ -14,16 +14,27 @@ public class Piece
 	private char blackOrWhite;
 
 	// Constructors
+	/**
+	 * This constructs a piece with default values.
+	 */
 	public Piece()
 	{
-
 	}
-
+	
+	/**
+	 * This constructs a player by creating a copy of another piece.
+	 * @param originalPiece is the original piece that will be copied
+	 */
 	public Piece(Piece originalPiece)
 	{
 		blackOrWhite = originalPiece.getBlackOrWhite();
 	}
 
+	/**
+	 * This constructs a player with a char.
+	 * @param letter this char given in letter is assigned 
+	 * 				 into the blackOrWhit instance variable
+	 */
 	public Piece(char letter)
 	{
 		this.blackOrWhite = letter;
