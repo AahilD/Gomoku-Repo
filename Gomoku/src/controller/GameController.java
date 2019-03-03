6eb128c13ec3d5dc4c90b64718d12e0c4e898f98
manucote <manu.cote1@gmail.com>
2019-03-03 14:56:59 -0700

    settup architecture for PVE controller and added TODO for Emily

--- Gomoku/src/controller/GameController.java
+++ Gomoku/src/controller/GameController.java
@@ -7,8 +7,55 @@ public abstract class GameController
 
     // TODO @Aahil make private variable private
     // TODO @Aahil remove roundCount (controllers will keep track of this)
-    static Game game;
-    static int roundCount;
-    static int turnCount;
-    private int roundCount;
+    private static Game game;
+    private static int roundCount;
+
+    public int getRoundCount()
+    {
+	return roundCount;
+    }
+
+    public Game getGame()
+    {
+	return game;
+    }
+
+    public void setGame(Game toGame)
+    {
+	game = toGame;
+    }
+
+    /**
+     * @param roundCount
+     */
+    public static void setRoundCount(int toRoundCount)
+    {
+	roundCount = toRoundCount;
+    }
+
+    /**
+     * Call this method to increment the value for round count.
+     */
+    public static void incrementRoundCount()
+    {
+	roundCount++;
+    }
+
+    /**
+     * This method will be invoked by the gui to indicate to the controller that
+     * the user wants to play an other round.
+     */
+    protected abstract void playAnotherRound();
+
+    /**
+     * This method will handle any request by the GUI to place a piece on the
+     * board at the given coordinates.
+     * 
+     * @param x is a variable of type int that represents the row (x coordinate)
+     *          of the square on the board.
+     * @param y is a variable of type int that represents the column (y
+     *          coordinate) of the square on the board.
+     */
+    protected abstract void playMove(int x, int y);
+
 }
