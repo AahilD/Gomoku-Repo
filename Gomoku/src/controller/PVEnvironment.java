f79aa7874b2cbc4bf0a423a79a1457cd0c279251
manucote <manu.cote1@gmail.com>
2019-03-05 18:48:34 -0700

    reviewed emilies todo's that were pending review, new comments added

--- Gomoku/src/controller/PVEnvironment.java
+++ Gomoku/src/controller/PVEnvironment.java
@@ -3,7 +3,8 @@ package controller;
 import java.util.ArrayList;
 import java.util.Random;
 
-import broker.Square; 
+import broker.Board;
+import broker.Square;
 
 /**
  * @author GROUP 22
@@ -28,7 +29,7 @@ public class PVEnvironment extends GameController
      * computer selects an available square at random
      */
     private ArrayList<int[]> playerMoves = new ArrayList<int[]>();
-    private String level; 
+    private String level;
 
     public static void initializeGame(String player1name, String player2name)
     {
@@ -66,52 +67,98 @@ public class PVEnvironment extends GameController
 	playMove(x, y);
     }
 
-    private void environmentPlayMove()
+    private void environmentPlayMove(int level)
     {
-	// TODO @Pending Review  : Using if statement to play appropriate method for level (I am
-    // assuming the level chosen is a string input and also assuming 3 levels of difficulty)  
+	// TODO @Emily yes of course I will have the gui pass in an int that
+	// will exactly equal to level in the method name
+	//
+	// FYI: perhaps using swtich {case #: methodcall_lvl_#(); break; case:
+	// #...} would make for cleaner coding style. If you are not familiar
+	// with switchcases I will provde an example in comments. I don't want
+	// to change your code, just change the reference value to be an int
+	// instead of a string.
+
+	/**
+	 * switch case statement example
+	 * 
+	 * switch(lvl) { case 0: environment_lvl_zero(); break; case 1:
+	 * environment_lvl_one(); break; case 2: environment_lvl_two(); break; }
+	 * 
+	 * Note: you can keep if statements if you so chose, I just thought I
+	 * would share with you a neat alternative. :) you chose what you
+	 * prefer.
+	 * 
+	 * Once you are done implementing this method remove any comments you no
+	 * longer need.
+	 */
 	/*
 	 * this method should check the level of difficulty and select the
 	 * appropriate method to play a move you can use if statements or case
 	 * statements or what ever you prefer. then you need to
 	 * 
 	 */
-    	if (level == "zero")
-    	{
-    		environment_lvl_zero();
-    	}
-    	//if (level == "one")
-    	//{
-    		//environment_lvl_one();
-    	//}
-    	//if (level == "two")
-    	//{
-    		//environment_lvl_two();
-    	//{
+	if (level == "zero")
+	{
+	    environment_lvl_zero();
+	}
+	// if (level == "one")
+	// {
+	// environment_lvl_one();
+	// }
+	// if (level == "two")
+	// {
+	// environment_lvl_two();
+	// {
     }
 
     private void environment_lvl_zero()
     {
-	// TODO @Pending review (not too sure which method to use to play the piece) 
+	// TODO @Emily yes playMove in the main controller is the correct
+	// method.
 	/*
 	 * This method should randomly chose any available square on the board
 	 * at random and play on that square. environment should always be
 	 * Player 2.
 	 */
-    	Random rand = new Random();
-    	int x = rand.nextInt(19);
-    	int y = rand.nextInt(19); 
-    	while (Square.isEmpty() == false)
-    	{
-    		x = rand.nextInt(19);
-    		y = rand.nextInt(19); 
-    	}
-    	if (Square.isEmpty()==true)
-    	{
-    		playMove(x,y);
-    	}
-    	
-    
+	Random rand = new Random();
+	int x = rand.nextInt(19);
+	int y = rand.nextInt(19);
+	// TODO @emily: Fix the followign.
+	/**
+	 * - you should not be making a static reference to Square - you need to
+	 * getGame().getCurrentBoard() - once you have the board you can check
+	 * to make sure your random coordinates target an available square to
+	 * play. - once a random coordinate has succesffully found an empty
+	 * square, it should attempt to play the move - to play the move you
+	 * 
+	 * Also: - isEmpty() is already returns a boolean checking to see if a
+	 * boolean is true or false is redundant, therfore all you need to do is
+	 * call the method. - you have an infinite loop here. Not sure why you
+	 * are using a while loop. - you should not hard code the value 19.
+	 * instead you should use the methods and variables already available to
+	 * determine these dimenssions. - you
+	 * 
+	 * Note: you can add/remove all the methods in this class as you see fit
+	 * to implement the logic. I think we should have one method that
+	 * overrides playmove in the parent class that will invoke the
+	 * environment to play a move. then there should be another private
+	 * method for the environment to play a move (if you do this you should
+	 * have this method make sure the square is available and play the move.
+	 * you can throw exceptions or return booleans as you see fit. but make
+	 * sure this method is reusable for the other levels keep difficulty
+	 * logic in here)))
+	 */
+
+	while (Square.isEmpty() == false)
+	{
+	    x = rand.nextInt(19);
+	    y = rand.nextInt(19);
+	}
+	if (Square.isEmpty() == true)
+	{
+	    playMove(x, y);
+	}
+
     }
 
     /**
