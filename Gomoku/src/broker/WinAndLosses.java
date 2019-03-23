b8ee5b8c9a1794f54f06eb75d63eafba1f89fcc7
Steven <stevenham00@gmail.com>
2019-03-23 16:17:32 -0600

    JavaDoc

--- Gomoku/src/broker/WinAndLosses.java
+++ Gomoku/src/broker/WinAndLosses.java
@@ -2,35 +2,61 @@ package broker;
 
 public class WinAndLosses extends Exception {
 
+	//Serial Version id
 	private static final long serialVersionUID = 1L;
 	
-	//Instance variables
+	//The Winner player object
 	private Player winner;
+	//The loser Player object
 	private Player loser;
 	
-	//Constructor
+	/**
+	 * This is the default constructor, which stores which player won and which one lost.
+	 * 
+	 * @param aWinner	The winning player
+	 * @param aLoser 	The loosing player
+	 */
 	public WinAndLosses (Player aWinner, Player aLoser)
 	{
 		winner = new Player(aWinner);
 		loser = new Player(aLoser);
 	}
 	
-	//Getters and setters
+	/**
+	 * This method return the winner
+	 * 
+	 * @return Copy of Player object of the winner
+	 */
 	public Player getWinner()
 	{
 		return new Player(winner);
 	}
 	
+	/**
+	 * This methods sets who the winner is
+	 * 
+	 * @param aPlayer	Player object of winner
+	 */
 	public void setWinner(Player aPlayer)
 	{
 		winner = new Player(aPlayer);
 	}
 	
+	/**
+	 * THis method returns the loser
+	 * 
+	 * @return	Copy of Player object for the loser
+	 */
 	public Player getLoser()
 	{
 		return new Player(loser);
 	}
 	
+	/**
+	 * This method sets who the loser is
+	 * 
+	 * @param aPlayer	Player object of loser
+	 */
 	public void setLoser(Player aPlayer)
 	{
 		loser = new Player(aPlayer);
